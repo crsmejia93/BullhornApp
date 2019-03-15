6944d330fc600047bdc65501d44abf46a434e525
GBTC408003UR <GBTC408003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-15 16:52:25 -0400

    Added Bootstrap

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,16 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 
 @Controller
@@ -21,7 +18,8 @@ public class HomeController {
     @Autowired
     MessageRepository messageRepository;
 
-//    private static final DateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+    @Autowired
+    CloudinaryConfig cloudc;
 
     @RequestMapping("/")
     public String listMessages(Model model){
@@ -36,14 +34,15 @@ public class HomeController {
     }
 
     @PostMapping("/process")
-    public String processForm(@Valid Message message, BindingResult result){
+    public String processForm(@Valid Message message, @RequestParam("file")MultipartFile file, BindingResult result){
         if(result.hasErrors()){
             return "messageform";
         }
         try{
             message.setPostedDate(LocalDateTime.now());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+            return "redirect:/add";
         }
         messageRepository.save(message);
         return "redirect:/";
